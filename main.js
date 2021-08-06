37983d57e6f71577540397c3414d23c8113cf00c
Rachid Oudouch <36020015+Ra-Wo@users.noreply.github.com>
2021-08-06 16:46:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js';
 import {
    OrbitControls
 } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
